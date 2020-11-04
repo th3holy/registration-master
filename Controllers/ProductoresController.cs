7039a599c74e62dc3093ba47906d544c9d8b74b3
SantaCruz <43019041+th3holy@users.noreply.github.com>
2020-11-04 11:26:39 -0300

    se agrego filtro por productor

---
+++ Controllers/ProductoresController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using registration.models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace registration.Controllers
+{
+    [Route("api/productores")]
+    [ApiController]
+    public class ProductoresController : ControllerBase
+    {
+        private readonly ModelContext context;
+
+        public ProductoresController( ModelContext context)
+        {
+            this.context = context;
+        }
+        // GET: api/<ProductoresController>
+        [HttpGet]
+        public IEnumerable<AspNetUsers> Get()
+        {
+            return context.AspNetUsers.Where(u =>u.TipoUsuario == "Productor").ToList();
+        }
+
+        //// GET api/<ProductoresController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/<ProductoresController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/<ProductoresController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<ProductoresController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ Controllers/UsuarioProductosController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using registration.models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace registration.Controllers
+{
+    [Route("api/usuarioProducto")]
+    [ApiController]
+    public class UsuarioProductosController : ControllerBase
+    {
+        private readonly ModelContext context;
+
+        public UsuarioProductosController(ModelContext context)
+        {
+            this.context = context;
+        }
+        // GET: api/<UsuarioProductosController>
+        [HttpGet("{id}")]
+        public IEnumerable<Product> Get(string id )
+        {
+            return context.Product.Where(u =>u.IdUser == id).ToList();
+        }
+
+        //// GET api/<UsuarioProductosController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // POST api/<UsuarioProductosController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<UsuarioProductosController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UsuarioProductosController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
